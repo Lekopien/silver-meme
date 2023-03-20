12554f09dc35d1d06806ecf8f5329d331092fcf2
Lekopien <51331171+Lekopien@users.noreply.github.com>
2023-03-21 00:21:04 +0300

    Delete camel.rb

--- camel.rb
+++
@@ -1,9 +0,0 @@
-def camelcase(s)
-  words = s.split
-  # capitalize the first letter of each word except for the first one, and join them together
-  words.map.with_index { |word, index| index == 0 ? word : word.capitalize }.join
-end
-
-# example usage:
-puts camelcase("hello case") # output: "HelloCase"
-puts camelcase("camel case word") # output: "CamelCaseWord"
